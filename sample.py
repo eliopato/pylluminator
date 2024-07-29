a6a6f5f2c485c71489fc33dd22119a0b68dbda92
elsa <elsa.fanchon@univ-amu.fr>
2024-07-29 11:15:43 +0200

    update sample df architecture

--- sample.py
+++ sample.py
@@ -22,7 +22,7 @@ class Samples:
         LOGGER.info(f'>> start reading sample files from {datadir}')
 
         for _, line in self.sheet.df.iterrows():
-            sample = Sample()
+            sample = Sample(line.sample_name)
             for channel in Channel:
                 pattern = f'*{line.sentrix_id}*{line.sentrix_position}*{channel}*.idat'
                 paths = [p.__str__() for p in Path(datadir).rglob(pattern)]
@@ -42,22 +42,32 @@ class Samples:
 
     def merge_manifest_info(self) -> None:
         LOGGER.info(f'>> start merging manifest and sample data frames')
-        for name, sample in self.samples.items():
-            LOGGER.info(f'merging sample {name}')
+        for sample in self.samples.values():
             sample.merge_manifest_info(self.annotation)
         LOGGER.info(f'done merging manifest and sample data frames\n')
 
-    def drop_optional_cols(self) -> None:
+    def infer_type1_channel(self, switch_failed=False, mask_failed=False) -> None:
+        LOGGER.info(f'>> start inferring probes')
         for sample in self.samples.values():
-            sample.drop_optional_cols()
+            sample.infer_type1_channel(switch_failed, mask_failed)
+        LOGGER.info(f'done inferring probes\n')
+
+    def get_betas(self) -> dict:
+        betas_dict = {}
+        LOGGER.info(f'>> start calculating betas')
+        for name, sample in self.samples.items():
+            betas_dict[name] = sample.get_betas()
+        LOGGER.info(f'done calculating betas\n')
+        return betas_dict
 
 
 class Sample:
 
-    def __init__(self):
+    def __init__(self, name: str):
         self.idata = dict()
         self.df = None
         self.full_df = None
+        self.name = name
 
     def set_idata(self, channel: Channel, dataset: IdatDataset) -> None:
         self.idata[channel] = dataset
@@ -65,15 +75,15 @@ class Sample:
     def merge_manifest_info(self, annotation: Annotations) -> None:
         """ Merge manifest and mask dataframes to idat information."""
 
+        LOGGER.info(f'merging sample {self.name}')
         channel_dfs = []
 
         for channel, idata in self.idata.items():
-            manifest_df = annotation.manifest
-            mask_df = annotation.mask
             idata_df = idata.probe_means
 
-            sample_df = idata_df.join(manifest_df, how='inner')
-            sample_df = sample_df.join(mask_df.drop(columns='probe_type'), on='probe_id', how='left')
+            sample_df = idata_df.join(annotation.manifest, how='inner')
+            sample_df = sample_df.join(annotation.mask.drop(columns='probe_type'), on='probe_id', how='left')
+
             lost_probes = len(sample_df) - len(idata_df)
             if lost_probes > 0:
                 LOGGER.warning(f'Lost {lost_probes} while merging information with Manifest (out of {len(idata_df)})')
@@ -84,13 +94,7 @@ class Sample:
             sample_df.loc[(sample_df.type == 'I') & (sample_df.index == sample_df.address_b), 'methylation_state'] = 'M'
             sample_df.loc[(sample_df.type == 'I') & (sample_df.index == sample_df.address_a), 'methylation_state'] = 'U'
 
-            # deduce which Type I probes are out-of-band (oob) and which ones are in-band (ib) by comparing the channel
-            # specified in the manifest with the file channel. Type II probes are set in-band.
             sample_df['signal_channel'] = channel.value[0]
-            sample_df['ib_or_oob'] = 'ib'
-            oob_indexes = (sample_df.type == 'I') & (sample_df.channel != sample_df.signal_channel)
-            sample_df.loc[oob_indexes, 'ib_or_oob'] = 'oob' + sample_df.loc[oob_indexes, 'signal_channel']
-            sample_df = sample_df.rename(columns={'channel': 'manifest_channel'})
 
             # to improve readability
             sample_df.loc[sample_df.probe_type == 'rs', 'probe_type'] = 'snp'
@@ -98,61 +102,82 @@ class Sample:
             channel_dfs.append(sample_df)
 
         self.full_df = pd.concat(channel_dfs)
-        self.df = self.drop_optional_cols()
+        # reshape dataframe to have something resembling sesame data structure - one row per probe
+        self.df = self.full_df.pivot(values='mean_value',
+                                     columns=['signal_channel', 'methylation_state'],
+                                     index=['type', 'channel', 'probe_type', 'probe_id'])
 
-    def get_betas(self):
+        # index column 'channel' corresponds by default to the manifest channel. But it could change by calling
+        # 'infer_type_i_channel()' e.g., so we need to keep track of the manifest_channel in another column
+        self.df['manifest_channel'] = self.df.index.get_level_values('channel').values
+
+    def get_betas(self) -> pd.DataFrame:
         # todo check why sesame gives the option to calculate separately R/G channels for Type I probes (sum.TypeI arg)
         # https://github.com/zwdzwd/sesame/blob/261e811c5adf3ec4ecc30cdf927b9dcbb2e920b6/R/sesame.R#L191
-        pivoted = self.df.pivot(values='mean_value', columns=['signal_channel', 'methylation_state'], index='probe_id')
-        pivoted = pivoted.fillna(0)
-        methylated_signal = pivoted['R', 'M'] + pivoted['G', 'M']
-        unmethylated_signal = pivoted['R', 'U'] + pivoted['G', 'U']
-        # set minimum values for each term as set in sesame
-        pivoted['beta'] = methylated_signal.clip(lower=1) / (methylated_signal + unmethylated_signal).clip(lower=2)
+        df = self.df.fillna(0)
+        methylated_signal = df['R', 'M'] + df['G', 'M']
+        unmethylated_signal = df['R', 'U'] + df['G', 'U']
+        # use clip function to set minimum values for each term as set in sesame
+        return methylated_signal.clip(lower=1) / (methylated_signal + unmethylated_signal).clip(lower=2)
 
-    def infer_typeI_channel(self, switch_failed=False, mask_failed=False) -> None:
-        LOGGER.info(' >> starting to infer probes')
-        pivoted = self.df[self.df.type == 'I'].pivot(values='mean_value',
-                                                     columns=['signal_channel', 'methylation_state'],
-                                                     index=['probe_id', 'manifest_channel'])
+    def infer_type1_channel(self, switch_failed=False, mask_failed=False) -> None:
+        """For Infinium type I probes, infer the channel from the signal values, setting it to the channel with the max
+        signal. If max values are equals, the channel is set to R (as opposed to G in sesame).
 
-        # infer new column values from maximum intensities
-        red_max = pivoted['R'].max(axis=1)
-        grn_max = pivoted['G'].max(axis=1)
-        pivoted['inferred_channel'] = np.where(red_max > grn_max, 'R', 'G')
+        switch_failed: if set to True, probes with NA values or whose max values are under a threshold (the 95th
+        percentile of the background signals) will be switched back to their original value
+
+        mask_failed: mask failed probes (same probes as switch_failed)"""
+
+        # subset to use
+        type1_df = self.df.loc['I'].droplevel('methylation_state', axis=1).copy()
+
+        # get the channel (provided by the index) where the signal is at its max for each probe
+        type1_df['inferred_channel'] = type1_df.idxmax(axis=1, numeric_only=True).values
 
         # handle failed probes
-        if not switch_failed:
-            # Calculate background max
-            bg_signal_values = np.concatenate([pivoted.loc[(pivoted.inferred_channel == 'R'), 'G'],
-                                               pivoted.loc[(pivoted.inferred_channel == 'G'), 'R']])
+        if not switch_failed or mask_failed:
+            # calculate background for type I probes
+            bg_signal_values = np.concatenate([type1_df.loc[type1_df.inferred_channel == 'R', 'G'],
+                                               type1_df.loc[type1_df.inferred_channel == 'G', 'R']])
             bg_max = np.percentile(bg_signal_values, 95)
-            # reset color channel to the value of 'manifest_channel' for failed indexes
-            failed_idxs = (red_max.isna() | grn_max.isna() | (red_max.combine(grn_max, max) < bg_max))
-            pivoted.loc[failed_idxs, 'inferred_channel'] = pivoted[failed_idxs].index.get_level_values(1).tolist()
+            failed_idxs = (type1_df.max(axis=1, numeric_only=True) < bg_max) | (type1_df.isna().any(axis=1))
+            LOGGER.info(f'number of failed probes switched back: \n{failed_idxs.groupby("channel").sum()}')
 
-        # todo
-        # if mask_failed:
-        #     sdf.loc[inf1_idx[idx], 'mask'] = True
+            if not switch_failed:
+                # reset color channel to the value of 'manifest_channel' for failed indexes of type I probes
+                type1_df.loc[failed_idxs, 'inferred_channel'] = type1_df.loc[failed_idxs, 'manifest_channel'].tolist()
+            # todo
+            # if mask_failed:
+            #     sdf.loc[inf1_idx[idx], 'mask'] = True
 
-        # update initial dataframes
-        inferred_channel_df = pivoted.reset_index('manifest_channel')['inferred_channel']
-        self.df = self.df.merge(inferred_channel_df, on='probe_id', how='left')
-        self.full_df = self.full_df.merge(inferred_channel_df, on='probe_id', how='left')
+        LOGGER.info(f"summary: \n{type1_df.groupby(['manifest_channel', 'inferred_channel']).count().max(axis=1)}")
 
-        LOGGER.info(pivoted['inferred_channel'].reset_index().groupby(['manifest_channel', 'inferred_channel']).count())
+        self.df.loc['I', 'inferred_channel'] = type1_df['inferred_channel'].values
 
-    def drop_optional_cols(self) -> pd.DataFrame | None:
-        """ Clear up the data frame to make it easier to read and only keep track of required values"""
-        if self.full_df is None:
-            LOGGER.warning('drop_optional_cols : please run merge_manifest_info() first')
-            return None
+        # make the inferred channel the new channel index
+        self.set_channel_index_as('inferred_channel', drop=True)
 
-        required_cols = ['probe_id', 'type', 'probe_type', 'manifest_channel', 'signal_channel', 'methylation_state',
-                         'ib_or_oob', 'mean_value']
-        missing_cols = [c for c in required_cols if c not in self.full_df.columns]
-        if len(missing_cols) > 0:
-            LOGGER.warning(f'drop_optional_cols : not all required cols where found in df. Missing cols {missing_cols}')
-            return None
+    def set_channel_index_as(self, column: str, drop=True):
+        """Use an existing column specified by argument `column` as the new `channel` index. To keep the column, set
+        `drop` to False"""
 
-        return self.full_df[required_cols]
+        if column not in self.df.columns:
+            LOGGER.error(f'column {column} not found in df ({self.df.columns})')
+            return
+
+        if 'channel' in self.df.columns:
+            LOGGER.warning('dropping existing column `channel`')
+            self.df.drop(column=['channel'], inplace=True)
+
+        if drop:
+            self.df.rename(columns={column: 'channel'}, inplace=True)
+        else:
+            self.df['channel'] = self.df[column]  # copy values in a new column
+
+        self.df = (self.df.droplevel('channel')
+                          .set_index('channel', append=True)
+                          .reorder_levels(['type', 'channel', 'probe_type', 'probe_id']))
+
+    def reset_channel_index(self):
+        self.set_channel_index_as('manifest_channel', False)
