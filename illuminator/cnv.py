cb3b9df9082ff54788b57e1a7d39fe6cf0efb994
elsa <elsa.fanchon@univ-amu.fr>
2024-08-30 19:48:19 +0200

    fix CNV to handle NAs + time optimization

--- illuminator/cnv.py
+++ illuminator/cnv.py
@@ -16,6 +16,96 @@ from illuminator.utils import get_resource_folder, get_files_matching, download_
 LOGGER = logging.getLogger(__name__)
 
 
+def copy_number_variation(sample: Sample, normal_samples: Samples | None = None) -> (pr.PyRanges, pd.DataFrame, pd.DataFrame):
+    """Perform copy number variation (CNV) and copy number segmentation for a sample
+
+    :param sample: (illuminator.Sample) sample to be analyzed
+    :param normal_samples: (optional, illuminator.Samples, default None) samples to use for normalization, of the same
+        array type and genome version of `sample`. If none is specified (default), default normalization samples will be
+        loaded from a database - but this only work for EPIC/hg38 and EPICv2/hg38; for other array versions, you
+        **need** to provide samples.
+
+    :return: tuple(pr.Pyranges, pd.DataFrame, pd.DataFrame) : (1) the bins coordinates as a pr.Pyranges object, (3) the
+        bins signal as a pd.DataFrame object, and (3) the segments as a pd.DataFrame object"""
+
+    # normalization samples
+    if normal_samples is None:
+        normal_samples = get_normalization_samples(sample.annotation)
+        if normal_samples is None:
+            LOGGER.error('Please provide samples to use as normalization')
+            return
+    if sample.annotation.array_type != normal_samples.annotation.array_type:
+        LOGGER.warning('Array types of input sample and normalization samples are different')
+    normal_samples = normal_samples.samples.values()
+
+    genome_info = sample.annotation.genome_info
+    probe_coords_df = sample.annotation.get_genomic_ranges()
+
+    # get total intensity per probe and drop unnecessary indexes
+    target_intensity = sample.get_total_ib_intensity()
+    target_intensity = target_intensity.reset_index(['channel', 'type', 'probe_type'], drop=True).dropna()
+
+    norm_intensities_list = [s.get_total_ib_intensity() for s in normal_samples]
+    norm_intensities = pd.concat(norm_intensities_list, axis=1)
+    norm_intensities = norm_intensities.reset_index(['channel', 'type', 'probe_type'], drop=True).dropna()
+
+    # keep only probes that are in all 3 files (target methylation, normalization methylation and genome ranges)
+    overlapping_probes = target_intensity.index.intersection(norm_intensities.index).intersection(probe_coords_df.index)
+    LOGGER.debug(f'Keeping {len(overlapping_probes)} overlapping probes')
+    target_intensity = target_intensity.loc[overlapping_probes]
+    norm_intensities = norm_intensities.loc[overlapping_probes]
+    probe_coords_df = probe_coords_df.loc[overlapping_probes]
+
+    LOGGER.info(f'Fitting the linear regression on normalization intensities')
+
+    X = norm_intensities.values
+    y = target_intensity.values
+    fitted_model = LinearRegression().fit(X, y)
+    predicted = np.maximum(fitted_model.predict(X), 1)
+    probe_coords_df['cnv'] = np.log2(target_intensity / predicted)
+
+    # make tiles
+    tile_width = 50000
+    tiles = pr.tile_genome(genome_info.seq_length, tile_width).reset_index(drop=True).sort_ranges()
+    diff_tiles = tiles.subtract_ranges(genome_info.gap_info).reset_index(drop=True)
+
+    # make bins
+    non_empty_coords = probe_coords_df[probe_coords_df.End > probe_coords_df.Start]  # remove 0-width ranges
+    probe_coords = pr.PyRanges(non_empty_coords)
+    diff_tiles = diff_tiles.count_overlaps(probe_coords.reset_index())
+
+    # merge small bins together, until they reach a minimum of 20 overlapping probes #todo optimize (10sec)
+    bins = merge_bins_to_minimum_overlap(diff_tiles, probe_coords, 20, 1)
+
+    # segment the signal
+    joined_pr = probe_coords.reset_index().join_ranges(bins, suffix='_bin')
+    signal_bins = joined_pr.groupby(['Chromosome', 'Start_bin', 'End_bin'])['cnv'].median().reset_index()
+    signal_bins['map_loc'] = ((signal_bins['Start_bin'] + signal_bins['End_bin']) / 2).astype(int)
+
+    # todo : improve this method (and optimize : 15sec)
+    cn_seg = linear_segment.segment(signal_bins.cnv.values.astype('double'),
+                                    labels=signal_bins.Chromosome.values,
+                                    method='cbs', shuffles=10000, p=0.0001)
+
+    # merge the segmentation information with the signal info
+    df_seg = pd.DataFrame(zip(cn_seg.starts, cn_seg.ends, cn_seg.labels), columns=['start', 'end', 'chromosome'])
+    seg_values = []
+
+    for chromosome in set(signal_bins.Chromosome):
+        chrom_df = signal_bins[signal_bins.Chromosome == chromosome].reset_index()
+        chrom_segs = df_seg[df_seg.chromosome == chromosome]
+        for seg_id, seg_value in chrom_segs.iterrows():
+            start_pos = chrom_df.loc[seg_value.start].map_loc
+            end_pos = chrom_df.loc[seg_value.end - 1].map_loc
+            nb_bins = len(chrom_df.loc[seg_value.start:seg_value.end - 1])
+            mean_cnv = chrom_df.loc[seg_value.start:seg_value.end - 1].cnv.mean()
+            seg_values.append([chromosome, seg_id, start_pos, end_pos, nb_bins, mean_cnv])
+
+    seg_df = pd.DataFrame(seg_values, columns=['chromosome', 'seg_id', 'start', 'end', 'nb_bins', 'mean_cnv'])
+
+    return probe_coords, signal_bins, seg_df.set_index('seg_id')
+
+
 def get_normalization_samples(annotation: Annotations):
     """Read from the package's data normalization samples data, depending on the array type.
     Only EPIC v2 and EPIC are supported for now"""
@@ -50,85 +140,6 @@ def get_normalization_samples(annotation: Annotations):
 
     LOGGER.error(f'No predefined normalization data for array {annotation.array_type}')
     return None
-
-
-def copy_number_variation(sample: Sample, normal_samples: Samples | None = None) -> (pr.PyRanges, pd.DataFrame, pd.DataFrame):
-    # normalization samples
-    if normal_samples is None:
-        normal_samples = get_normalization_samples(sample.annotation)
-        if normal_samples is None:
-            LOGGER.error('Please provide samples to use as normalization')
-            return
-    if sample.annotation.array_type != normal_samples.annotation.array_type:
-        LOGGER.warning('Array types of input sample and normalization samples are different')
-
-    genome_info = sample.annotation.genome_info
-    probe_coords_df = sample.annotation.get_genomic_ranges()
-
-    # get total intensity per probe and drop unnecessary indexes
-    target_intensity = sample.get_total_ib_intensity()
-    target_intensity = target_intensity.dropna()
-    target_intensity = target_intensity.reset_index(['channel', 'type', 'probe_type'], drop=True)
-
-    norm_intensities_list = [s.get_total_ib_intensity() for s in normal_samples.samples.values()]
-    norm_intensities = pd.concat(norm_intensities_list, axis=1)
-    norm_intensities = norm_intensities.reset_index(['channel', 'type', 'probe_type'], drop=True)
-
-    # keep only probes that are in all 3 files (target methylation, normalization methylation and genome ranges)
-    overlapping_probes = [p for p in target_intensity.index if
-                          p in norm_intensities.index and p in probe_coords_df.index]
-    LOGGER.debug(f'Keeping {len(overlapping_probes)} overlapping probes')
-    target_intensity = target_intensity.loc[overlapping_probes]
-    norm_intensities = norm_intensities.loc[overlapping_probes]
-    probe_coords_df = probe_coords_df.loc[overlapping_probes]
-
-    LOGGER.info(f'Fitting the linear regression on normalization intensities')
-    X = norm_intensities.values
-    y = target_intensity.values
-    fitted_model = LinearRegression().fit(X, y)
-    predicted = np.maximum(fitted_model.predict(X), 1)
-    probe_coords_df['cnv'] = np.log2(target_intensity / predicted)
-
-    # make tiles
-    tile_width = 50000
-    tiles = pr.tile_genome(genome_info.seq_length, tile_width).reset_index(drop=True).sort_ranges()
-    diff_tiles = tiles.subtract_ranges(genome_info.gap_info).reset_index(drop=True)
-
-    # make bins
-    non_empty_coords = probe_coords_df[probe_coords_df.End > probe_coords_df.Start]  # remove 0-width ranges
-    probe_coords = pr.PyRanges(non_empty_coords)
-    diff_tiles = diff_tiles.count_overlaps(probe_coords.reset_index())
-
-    # merge small bins together, until they reach a minimum of 20 overlapping probes
-    bins = merge_bins_to_minimum_overlap(diff_tiles, probe_coords, 20, 1)
-
-    # segment the signal
-    joined_pr = probe_coords.reset_index().join_ranges(bins, suffix='_bin')
-    signal_bins = joined_pr.groupby(['Chromosome', 'Start_bin', 'End_bin'])['cnv'].median().reset_index()
-    signal_bins['map_loc'] = ((signal_bins['Start_bin'] + signal_bins['End_bin']) / 2).astype(int)
-
-    # todo : improve this method
-    cn_seg = linear_segment.segment(signal_bins.cnv.values.astype('double'),
-                                    labels=signal_bins.Chromosome.values,
-                                    method='cbs', shuffles=10000, p=0.0001)
-
-    # merge the segmentation information with the signal info
-    df_seg = pd.DataFrame(zip(cn_seg.starts, cn_seg.ends, cn_seg.labels), columns=['start', 'end', 'chromosome'])
-    seg_values = []
-
-    for chromosome in set(signal_bins.Chromosome):
-        chrom_df = signal_bins[signal_bins.Chromosome == chromosome].reset_index()
-        chrom_segs = df_seg[df_seg.chromosome == chromosome]
-        for seg_id, seg_value in chrom_segs.iterrows():
-            start_pos = chrom_df.loc[seg_value.start].map_loc
-            end_pos = chrom_df.loc[seg_value.end - 1].map_loc
-            nb_bins = len(chrom_df.loc[seg_value.start:seg_value.end - 1])
-            mean_cnv = chrom_df.loc[seg_value.start:seg_value.end - 1].cnv.mean()
-            seg_values.append([chromosome, seg_id, start_pos, end_pos, nb_bins, mean_cnv])
-
-    seg_df = pd.DataFrame(seg_values, columns=['chromosome', 'seg_id', 'start', 'end', 'nb_bins', 'mean_cnv'])
-
-    return probe_coords, signal_bins, seg_df.set_index('seg_id')
 
 
 def merge_bins_to_minimum_overlap(pr_to_merge: pr.PyRanges, pr_to_overlap_with: pr.PyRanges, minimum_overlap=20,
