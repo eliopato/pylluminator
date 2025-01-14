5b43e9ec37623bcc452f8d39e966dffd3d3e1eaa
elio <eliof@posteo.net>
2025-01-14 16:39:35 +0100

    update read_sample and samples functions tests

--- tests/test_read_samples.py
+++ tests/test_read_samples.py
@@ -47,13 +47,93 @@ def test_read_samples(data_path):
     assert my_samples.min_beads == min_beads
     assert len(my_samples.idata ) == max_samples
     assert my_samples.nb_samples == max_samples
-    assert len(my_samples._masked_indexes_per_sample) == max_samples
     assert my_samples.annotation.array_type == ArrayType.HUMAN_EPIC_V2
 
     # Check that the samples are correctly loaded
     sample = my_samples['PREC_500_3']
-    assert sample is not None
     assert len(sample) == 937688  # vs 937690 in the original data bc of 2 staining control probes with NA channels
+    assert sample is not None
+
+    # Check that the samples are correctly loaded
+    assert len(my_samples.type1()) == 128295
+    assert len(my_samples.type1_green()) == 45685
+    assert len(my_samples.type1_red()) == 82610
+    assert len(my_samples.type2()) == 809393
+    assert len(my_samples.type1()['PREC_500_3'].columns) == 4
+    assert len(my_samples.type2()['PREC_500_3'].columns) == 2
+    # test probe types
+    assert len(my_samples.cg_probes()) == 933252
+    assert len(my_samples.ch_probes()) == 2914
+    assert len(my_samples.snp_probes()) == 65
+    # test meth and unmeth subsets
+    assert len(my_samples.unmeth()) == 937688
+    assert len(my_samples.meth()) == 937688
+    assert len(my_samples.unmeth()['PREC_500_3'].columns) == 2
+    assert len(my_samples.meth()['PREC_500_3'].columns) == 2
+
+    ##################################################################################"
+
+    # test type 1 out of band
+    oob_probes = my_samples.oob()['PREC_500_3']
+    assert len(oob_probes) == 128295
+    oob_r = oob_probes.xs('cg00001261_BC11', level='probe_id').values[0]
+    assert len(oob_r) == 4
+    assert oob_r[0] == 305
+    assert oob_r[1] == 346
+    assert np.isnan(oob_r[2])
+    assert np.isnan(oob_r[3])
+    oob_g = oob_probes.xs('rs1414097_BC11', level='probe_id').values[0]
+    assert len(oob_g) == 4
+    assert np.isnan(oob_g[0])
+    assert np.isnan(oob_g[1])
+    assert oob_g[2] == 277
+    assert oob_g[3] == 241
+
+    # out of band red
+    assert len(my_samples.oob_red()) == 45685
+    oob_r = my_samples.oob_red().xs('rs1414097_BC11', level='probe_id')['PREC_500_3'].values[0]
+    assert len(oob_r) == 2
+    assert oob_r[0] == 277
+    assert oob_r[1] == 241
+
+    # out of band green
+    assert len(my_samples.oob_green()) == 82610
+    oob_g = my_samples.oob_green().xs('cg00001261_BC11', level='probe_id')['PREC_500_3'].values[0]
+    assert len(oob_g) == 2
+    assert oob_g[0] == 305
+    assert oob_g[1] == 346
+
+    ##################################################################################"
+
+    # test type 1 in band
+    ib_probes = my_samples.ib()['PREC_500_3']
+    assert len(ib_probes) == 128295
+    ib_r = ib_probes.xs('cg00001261_BC11', level='probe_id').values[0]
+    assert len(ib_r) == 4
+    assert np.isnan(ib_r[0])
+    assert np.isnan(ib_r[1])
+    assert ib_r[2] == 5396
+    assert ib_r[3] == 11840
+    ib_g = ib_probes.xs('rs1414097_BC11', level='probe_id').values[0]
+    assert len(ib_g) == 4
+    assert ib_g[0] == 636
+    assert ib_g[1] == 687
+    assert np.isnan(ib_g[2])
+    assert np.isnan(ib_g[3])
+    # in band red
+    assert len(my_samples.ib_red()) == 82610
+    ib_r = my_samples.ib_red().xs('cg00001261_BC11', level='probe_id')['PREC_500_3'].values[0]
+    assert len(ib_r) == 2
+    assert ib_r[0] == 5396
+    assert ib_r[1] == 11840
+    # in band green
+    assert len(my_samples.ib_green()) == 45685
+    ib_g = my_samples.ib_green().xs('rs1414097_BC11', level='probe_id')['PREC_500_3'].values[0]
+    assert len(ib_g) == 2
+    assert ib_g[0] == 636
+    assert ib_g[1] == 687
+
+    ##################################################################################"
 
     # check values for a probe of each type (Type I green, type I red, type II)
     probe_tI_r = my_samples._signal_df.xs('cg00003555_BC11', level='probe_id')['PREC_500_3']
@@ -73,25 +153,3 @@ def test_read_samples(data_path):
     assert probe_tII_g[('R', 'M')].values == 319.0
     assert probe_tII_g[('G', 'U')].values == 2827.0
     assert probe_tII_g[('G', 'M')].values == 1522.0
-
-    my_samples.calculate_betas()
-    my_samples.type1()
-    my_samples.type2()
-    my_samples.ib()
-    my_samples.ib_red()
-    my_samples.ib_green()
-    my_samples.oob_red()
-    my_samples.oob_green()
-    my_samples.meth()
-    my_samples.unmeth()
-    my_samples.type1_red()
-    my_samples.type1_green()
-    my_samples.cg_probes()
-    my_samples.ch_probes()
-    my_samples.snp_probes()
-    print('ok')
-    #
-    # assert len(my_samples.type1(False))
-    # assert len(my_samples.type2(False))
-
-
